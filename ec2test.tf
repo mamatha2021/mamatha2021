690e1b13490418f8655d46197a6f2c5b0af64147
mamatha2021 <87364438+mamatha2021@users.noreply.github.com>
2023-02-22 11:47:33 +0530

    Update ec2test.tf

--- ec2test.tf
+++ ec2test.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region              = "ap-southeast-1"
-  allowed_account_ids = ["489213317621"]
-  access_key          = "AKIAXDZ3C2H23FIDCCSR"
-  secret_key          = "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"
+  allowed_account_ids = ["332514269665"]
+  access_key          = "AKIAU223FDHQ72FWNVFP"
+  secret_key          = "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"
   default_tags {
     tags = {
       Name = "default tags"
@@ -12,7 +12,7 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0688ba7eeeeefe3cd"
   instance_type = "t3.micro"
 
   tags = {
